28fd57be1ff081e81632ad8589b7988ca502769f
Luke Hill <lukehill_uk@hotmail.com>
2023-11-15 16:15:10 +0000

    Fix RSpec/NamedSubject

--- ruby/spec/html-formatter/assets_loader_spec.rb
+++ ruby/spec/html-formatter/assets_loader_spec.rb
@@ -1,7 +1,7 @@
 require 'cucumber/html_formatter/assets_loader'
 
 describe Cucumber::HTMLFormatter::AssetsLoader do
-  let(:subject) { Cucumber::HTMLFormatter::AssetsLoader.new }
+  subject(:assets_loader) { Cucumber::HTMLFormatter::AssetsLoader.new }
 
   before do
     allow(File).to receive(:read).and_return('whatever content')
@@ -9,21 +9,21 @@ describe Cucumber::HTMLFormatter::AssetsLoader do
 
   context '.template' do
     it 'reads the content of assets/index.mustache.html' do
-      expect(subject.template).to eq('whatever content')
+      expect(assets_loader.template).to eq('whatever content')
       expect(File).to have_received(:read).with(/.*\/assets\/index\.mustache\.html$/)
     end
   end
 
   context '.css' do
     it 'reads the content of assets/main.css' do
-      expect(subject.css).to eq('whatever content')
+      expect(assets_loader.css).to eq('whatever content')
       expect(File).to have_received(:read).with(/.*\/assets\/main\.css$/)
     end
   end
 
   context '.script' do
     it 'reads the content of assets/main.js' do
-      expect(subject.script).to eq('whatever content')
+      expect(assets_loader.script).to eq('whatever content')
       expect(File).to have_received(:read).with(/.*\/assets\/main\.js$/)
     end
   end
--- ruby/spec/html_formatter_spec.rb
+++ ruby/spec/html_formatter_spec.rb
@@ -1,38 +1,40 @@
 require 'cucumber/messages'
 require 'cucumber/html_formatter'
 
-class FakeAssets
-  def template
-    "<html>{{css}}<body>{{messages}}</body>{{script}}</html>"
-  end
-
-  def css
-    "<style>div { color: red }</style>"
-  end
-
-  def script
-    "<script>alert('Hi')</script>"
-  end
-end
-
 describe Cucumber::HTMLFormatter::Formatter do
-  let(:subject) {
+  subject(:formatter) do
     formatter = Cucumber::HTMLFormatter::Formatter.new(out)
     allow(formatter).to receive(:assets_loader).and_return(assets)
     formatter
-  }
+  end
+
   let(:out) { StringIO.new }
-  let(:assets) { FakeAssets.new }
+  let(:fake_assets) do
+    Class.new do
+      def template
+        "<html>{{css}}<body>{{messages}}</body>{{script}}</html>"
+      end
+
+      def css
+        "<style>div { color: red }</style>"
+      end
+
+      def script
+        "<script>alert('Hi')</script>"
+      end
+    end
+  end
+  let(:assets) { fake_assets.new }
 
   context '.write_pre_message' do
     it 'outputs the content of the template up to {{messages}}' do
-      subject.write_pre_message()
+      formatter.write_pre_message()
       expect(out.string).to eq("<html>\n<style>div { color: red }</style>\n<body>\n")
     end
 
     it 'does not write the content twice' do
-      subject.write_pre_message()
-      subject.write_pre_message()
+      formatter.write_pre_message()
+      formatter.write_pre_message()
 
       expect(out.string).to eq("<html>\n<style>div { color: red }</style>\n<body>\n")
     end
@@ -46,13 +48,13 @@ describe Cucumber::HTMLFormatter::Formatter do
     end
 
     it 'appends the message to out' do
-      subject.write_message(message)
+      formatter.write_message(message)
       expect(out.string).to eq(message.to_json)
     end
 
     it 'adds commas between the messages' do
-      subject.write_message(message)
-      subject.write_message(message)
+      formatter.write_message(message)
+      formatter.write_message(message)
 
       expect(out.string).to eq("#{message.to_json},\n#{message.to_json}")
     end
@@ -60,7 +62,7 @@ describe Cucumber::HTMLFormatter::Formatter do
 
   context '.write_post_message' do
     it 'outputs the template end' do
-      subject.write_post_message()
+      formatter.write_post_message()
       expect(out.string).to eq("</body>\n<script>alert('Hi')</script>\n</html>")
     end
   end
@@ -73,7 +75,7 @@ describe Cucumber::HTMLFormatter::Formatter do
     end
 
     it 'produces the full html report' do
-      subject.process_messages([message])
+      formatter.process_messages([message])
       expect(out.string).to eq([
         '<html>',
         '<style>div { color: red }</style>',
