04404549392cce0336a693460c9e811db6285edf
Luke Hill <lukehill_uk@hotmail.com>
2023-11-15 16:27:47 +0000

    Tidy up lots of redundant code in assets_loader_spec

--- ruby/spec/html-formatter/assets_loader_spec.rb
+++ ruby/spec/html-formatter/assets_loader_spec.rb
@@ -1,30 +1,33 @@
 # frozen_string_literal: true
 
 describe Cucumber::HTMLFormatter::AssetsLoader do
-  subject(:assets_loader) { Cucumber::HTMLFormatter::AssetsLoader.new }
+  subject(:assets_loader) { described_class.new }
 
   before do
     allow(File).to receive(:read).and_return('whatever content')
   end
 
-  context '.template' do
+  describe '#template' do
     it 'reads the content of assets/index.mustache.html' do
-      expect(assets_loader.template).to eq('whatever content')
-      expect(File).to have_received(:read).with(/.*\/assets\/index\.mustache\.html$/)
+      expect(File).to receive(:read).with(/.*\/assets\/index\.mustache\.html$/)
+
+      assets_loader.template
     end
   end
 
-  context '.css' do
+  describe '#css' do
     it 'reads the content of assets/main.css' do
-      expect(assets_loader.css).to eq('whatever content')
-      expect(File).to have_received(:read).with(/.*\/assets\/main\.css$/)
+      expect(File).to receive(:read).with(/.*\/assets\/main\.css$/)
+
+      assets_loader.css
     end
   end
 
-  context '.script' do
+  describe '#script' do
     it 'reads the content of assets/main.js' do
-      expect(assets_loader.script).to eq('whatever content')
-      expect(File).to have_received(:read).with(/.*\/assets\/main\.js$/)
+      expect(File).to receive(:read).with(/.*\/assets\/main\.js$/)
+
+      assets_loader.script
     end
   end
 end
