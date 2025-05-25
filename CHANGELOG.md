08e096334dbf98dc0b2668696985bc0597b5f15c
David Goss <david@davidgoss.co>
2025-05-25 18:19:51 +0100

    Upgrade react-components to 23.0.0 (#369)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
+### Changed
+- Upgrade `react-components` to [23.0.0](https://github.com/cucumber/react-components/releases/tag/v23.0.0)
 
 ## [21.10.1] - 2025-04-19
 ### Changed
--- javascript/src/main.tsx
+++ javascript/src/main.tsx
@@ -1,27 +1,34 @@
 import './styles.scss'
 
-import * as messages from '@cucumber/messages'
-import { components, searchFromURLParams } from '@cucumber/react-components'
+import { Envelope } from '@cucumber/messages'
+import {
+  EnvelopesProvider,
+  ExecutionSummary,
+  FilteredDocuments,
+  SearchBar,
+  UrlSearchProvider,
+} from '@cucumber/react-components'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 
-const { CucumberReact } = components
-const { FilteredResults, EnvelopesWrapper, SearchWrapper } = components.app
-
 declare global {
   interface Window {
-    CUCUMBER_MESSAGES: messages.Envelope[]
+    CUCUMBER_MESSAGES: Envelope[]
   }
 }
 
 const root = createRoot(document.getElementById('content') as HTMLElement)
 
 root.render(
-  <CucumberReact theme="auto">
-    <EnvelopesWrapper envelopes={window.CUCUMBER_MESSAGES}>
-      <SearchWrapper {...searchFromURLParams()}>
-        <FilteredResults className="html-formatter" />
-      </SearchWrapper>
-    </EnvelopesWrapper>
-  </CucumberReact>
+  <EnvelopesProvider envelopes={window.CUCUMBER_MESSAGES}>
+    <UrlSearchProvider>
+      <div id="report" className="html-formatter">
+        <div className="html-formatter__header">
+          <ExecutionSummary />
+          <SearchBar />
+        </div>
+        <FilteredDocuments />
+      </div>
+    </UrlSearchProvider>
+  </EnvelopesProvider>
 )
