ad61940547b4e1156bfad57fb2402419c1d51719
David Goss <david@davidgoss.co>
2025-11-13 10:37:15 +0000

    Improve screenshot tests (#454)

--- javascript/playwright.config.ts
+++ javascript/playwright.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
   use: {
     headless: true,
     baseURL: 'http://localhost:3000',
+    colorScheme: 'light'
   },
   projects: [
     {
@@ -26,7 +27,6 @@ export default defineConfig({
   expect: {
     toHaveScreenshot: {
       stylePath: './test/screenshot.css',
-      maxDiffPixelRatio: 0.05,
     },
   },
 })
